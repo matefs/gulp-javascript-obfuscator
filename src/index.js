a83ebc2e87fa996925905f3dbc6c3549f292fd9d
Mateus Schverz <matefs8569@gmail.com>
2024-04-05 17:45:00 -0300

    add: npm start script into package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const javascriptObfuscator = require('gulp-javascript-obfuscator');
 
-gulp.src('file.js')
+gulp.src('src/file.js')
     .pipe(javascriptObfuscator())
     .pipe(gulp.dest('../dist'));
 
